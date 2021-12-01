831a5afef543d7e9e4ff0a783b776af087fb977b
bhattabhi013 <bhattabhi013@gmail.com>
2021-12-01 17:42:39 +0530

    introduction to widgets

--- lib/main.dart
+++ lib/main.dart
@@ -2,18 +2,54 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 
-void main() {
-    runApp(
-      demoApp()
-    );
-}
+// void main() {
+//     runApp(
+//       demoApp()
+//     );
+// }
+
+void main() => runApp(demoApp());
 
 class demoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:  Text('hello'),
+      home: Scaffold (
+        appBar :  returHeader(),
+        body:  returnBody(),
+      ),
     );
   }
 }
 
+returHeader(){
+  return AppBar(
+    title:  Text(
+      'Demo App'
+    ),
+  );
+}
+
+returnBody(){
+    var questions = ["what's your favourite color"
+  ,"What's your favourite animal"];
+  return Column(children:  [
+    Text(questions.elementAt(0),
+    ),
+    RaisedButton(child: Text('yes'),
+    onPressed: printmsg
+    ),
+    RaisedButton(child: Text('no'),
+    onPressed: printmsg
+    ),
+    // ElevatedButton(onPressed: printmsg, 
+    // child: Text('yep'),
+    // ),
+    // ),
+  ],
+  );
+}
+
+printmsg(){
+  print('buttonpressed');
+}
